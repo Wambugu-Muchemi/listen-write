a6e03b0474b9370a2a0ba254e33abe77d7f12813
Wambugu-Muchemi <wambugumuchemi2021@gmail.com>
2023-11-07 12:03:56 +0300

    code optimization, error handling and readability

--- EscucharUnSusurro/susurro/summarizeAI.py
+++ EscucharUnSusurro/susurro/summarizeAI.py
@@ -1,30 +1,44 @@
 from langchain.docstore.document import Document 
 from langchain.llms import OpenAI
-#from langchain.qa import load_qa_chain
 from langchain.chains.question_answering import load_qa_chain
 import json
 import requests
 from dotenv import load_dotenv
-load_dotenv() 
 import os
 from pprint import pprint
 
+load_dotenv() 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 PALM = os.getenv("PALM")
-def escribirAI(transcription):
 
+def escribir_ai(transcription):
+    """
+    Generate a summary and issue category from a transcription.
+
+    Args:
+    - transcription: The transcription text.
+
+    Returns:
+    - A tuple containing the summary and issue category.
+    """
     doc = Document(page_content=transcription)
     llm = OpenAI()
     qa_chain = load_qa_chain(llm, chain_type="stuff")
-    summary = qa_chain.run(input_documents=[doc],  
-                      question="This is a transcription made from a customer care call at Konnect Wifi which is an ISP company, please try to summarize what is going on in English. Please note, the transcription was made using Whisper and some parts may seem hazy due to audio problems, However try as much as you can by combining all segments and figuring out. Another thing to note , being an ISP company make note of some words despite wrong spelling may be related to routers (whisper keeps pronouncing router as 'ruta' so if you see ruta know its router), devices, WIFI,portal and so on")
-    issue_category = askpalm(f"{summary} Categorize this summary as any of the following: Router Installation , Change / Add Device ,Product Enquiry ,TV Connection ,SMS Code Issue ,Payment Issue, Router Technical Problem, Obtaining IP Issue, Connection Speed Issue, Login Challenge or Other")
+
+    summary = qa_chain.run(
+        input_documents=[doc],
+        question="This is a transcription made from a customer care call at Konnect Wifi which is an ISP company, please try to summarize what is going on in English. Please note, the transcription was made using Whisper and some parts may seem hazy due to audio problems, However try as much as you can by combining all segments and figuring out. Another thing to note, being an ISP company make note of some words despite wrong spelling may be related to routers (whisper keeps pronouncing router as 'ruta' so if you see ruta know its router), devices, WIFI, portal and so on"
+    )
+
+    issue_category = ask_palm(f"{summary} Categorize this summary as any of the following: Router Installation, Change / Add Device, Product Enquiry, TV Connection, SMS Code Issue, Payment Issue, Router Technical Problem, Obtaining IP Issue, Connection Speed Issue, Login Challenge or Other")
+
     print(issue_category)
     print(summary)
     return summary, issue_category
 
-def askpalm(prompt):
-    """Generates text using the Generative Language API.
+def ask_palm(prompt):
+    """
+    Generates text using the Generative Language API.
 
     Args:
         prompt: A string containing the prompt for text generation.
@@ -33,23 +47,25 @@ def askpalm(prompt):
         A string containing the generated text.
     """
     print("Calling Palmer for help!")
+
     headers = {'Content-Type': 'application/json'}
     data = {'prompt': {'text': prompt}}
-    response = requests.post(
-        f'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key={PALM}',
-        headers=headers,
-        data=json.dumps(data))
-    response.raise_for_status()
 
-    # Parse the response as JSON
-    gentext = response.json()
+    try:
+        response = requests.post(
+            f'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key={PALM}',
+            headers=headers,
+            data=json.dumps(data)
+        )
+        response.raise_for_status()
 
-    # Extract the generated text from the response
-    ans = gentext['candidates'][0]['output']
+        # Parse the response as JSON
+        gentext = response.json()
 
-    return ans
+        # Extract the generated text from the response
+        ans = gentext['candidates'][0]['output']
+        return ans
 
-# transcription = "File: segment_1.wav (Language: sw):K kFile: segment_2.wav (Language: sw):, kwa hivyo, kFile: segment_3.wav (Language: sw):Kwanzaa ni confirm, niwaneka mando inao Pana, siyo yonamba Ukona yoruta, ukona yoruta, konekti wa nyumba Namba kani hulipena kwa agent 0742257616 Pantone Wachani angalilie kuyo agent waka kujo kusaidia"
-# #escribirAI(transcription)
-# ans = askpalm(f"Please summarize. Here it is= '{transcription}")
-# pprint(ans)
+    except requests.exceptions.RequestException as e:
+        print(f"Error calling Palmer API: {e}")
+        return ''
